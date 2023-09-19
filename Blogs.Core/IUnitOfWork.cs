bef12cbb8a568223eb091059cedef63442bae90f
Adrian Jose Pinto <adrianpintoNet@outlook.es>
2023-09-19 09:38:29 -0300

    Created UnitOfWork implementation and pattern

---
+++ Blogs.Core/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Blogs.Core.Repositories.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public void Commit();
+        public void Rollback();
+        IUserRepository UserRepository { get; }
+        IBlogPostsRepository BlogPostsRepository { get; }
+        ICommentsRepository CommentRepository { get; }
+    }
+}
---
+++ Blogs.Core/UnitOfWork.cs
@@ -0,0 +1,73 @@
+﻿using Blogs.Core.DataContexts;
+using Blogs.Core.Repositories;
+using Blogs.Core.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.Core
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly BlogPostsApiContext _context;
+        private bool _disposed;
+
+        public IUserRepository UserRepository { get; }
+        public IBlogPostsRepository BlogPostsRepository { get; }
+        public ICommentsRepository CommentRepository { get; }
+
+        public UnitOfWork(BlogPostsApiContext context, IUserRepository userRepository,
+            IBlogPostsRepository blogPostsRepository,
+            ICommentsRepository commentsRepository)
+        {
+            _context = context;
+            UserRepository = userRepository;
+            BlogPostsRepository = blogPostsRepository;
+            CommentRepository = commentsRepository;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+            foreach (var entry in _context.ChangeTracker.Entries()
+                .Where(e => e.State != EntityState.Unchanged))
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Modified:
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
