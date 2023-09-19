8d18e4945422156f22a85ff310908fa216a78754
Adrian Jose Pinto <adrianpintoNet@outlook.es>
2023-09-19 09:59:58 -0300

    Created Application layer and DTO classes to communicate with the Core.

---
+++ Blogs.Application/DTO/BlogPost/BlogPostResultDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.Application.DTO.BlogPost
+{
+    public class BlogPostResultDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string HtmlContent { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreationUsername { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string UpdateUsername { get; set; }
+    }
+}
---
+++ Blogs.Application/DTO/Comment/CommentResultDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogs.Application.DTO.Comment
+{
+    public class CommentResultDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public Guid BlogPostId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreationUsername { get; set; }
+        public DateTime UpdateDateTime { get; set; }
+        public string UpdateUsername { get; set; }
+    }
+}
